ad12c8b31901e81417b6b172dbfe6b4d196ff605
varunks99 <varunshiri99@gmail.com>
2020-03-26 23:32:47 +0530

    List page in making

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,23 @@
 module.exports = (app, passport) => {
-    var AddressAutocomplete = require('google-address-autocomplete');
-    app.get('/', (req, res) => {
-        res.render('index')
-    })
-    app.post('/', (req, res) => {
-        placeid = req.body.resultid
-        res.send(placeid)
-    })
+  var AddressAutocomplete = require('google-address-autocomplete');
+  app.get('/', (req, res) => {
+    res.render('index')
+  })
+  app.post('/', (req, res) => {
+    placeid = req.body.resultid
+    res.send(placeid)
+  })
 
-    app.get('/add-store', (req, res) => {
-        res.render('add-store')
-    })
+  app.get('/add-store', (req, res) => {
+    res.render('add-store')
+  })
 
-    app.get('/test', (req, res) => {
-        res.render('test')
-    })
+  app.get('/list-stores', (req, res) => {
+    res.render('list')
+  })
+
+  app.get('/test', (req, res) => {
+    res.render('test')
+  })
 
 }
